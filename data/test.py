99eba01187d8ac827bb98bf9beeb3f301273bb06
Ignatov.AI <ignatov-ai@yandex.ru>
2023-06-06 14:35:54 +0300

    ПОКА НЕ ИСПРАВЛЕН ОБРАБОЧИК ВСТАВКИ ПУСТЫХ ПЕРИОДОВ, например нет АТТ1, а есть только АТТ2

---
+++ data/test.py
@@ -0,0 +1,9 @@
+from math import ceil
+
+a = 4
+b = 5
+s = (a + b) / 2
+
+avg = ceil(s)
+
+print(s, avg)
--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import pandas as pd
 import openpyxl
+from openpyxl.styles import Alignment, PatternFill, Font, Fill, NamedStyle, Border
+from math import ceil
 
 ### Чтение периодов из выгрузки ###
 
@@ -16,12 +18,13 @@ url_period_2 = pd.ExcelFile('data/10Я-2.xlsx')
 
 if periods == 3:
     url_period_3 = pd.ExcelFile('data/10Я-3.xlsx')
-
-# Открытие файла с итоговыми оценками за периоды
 url_all_periods = pd.ExcelFile('data/10Я-все.xlsx')
 
 
-book = openpyxl.load_workbook(filename='data/10Я-все.xlsx')
+# Открытие файла с итоговыми оценками за периоды
+file_name_all_periods = '10Ч-Костина-все'
+
+book = openpyxl.load_workbook(filename='data/' + file_name_all_periods + '.xlsx')
 sheet = book.worksheets[0]
 
 # создаем пустой список для хранения данных из таблицы
@@ -48,24 +51,36 @@ predmeti = ';'.join(data[0])
 
 predmeti_p1 = ['']
 for i in range(len(data[2])):
-    if data[2][i] == 'Аттестационный период 1':
+    if data[2][i] == 'Год':
         predmeti_p1.append(data[0][i])
 
 # Трассировка вывода предметов
 # print(predmeti_p1)
 
 # создание книги для проверенной выгрузки
-
 out_book = openpyxl.Workbook()
 out_book.remove(out_book.active)
 out_sheet = out_book.create_sheet("Проверка ГОД")
+out_sheet = out_book.active
+out_sheet.column_dimensions['A'].width = 30
+
+for col in 'BCDEFGHIJKLMNOPQRSTUVWXWZ':
+    out_sheet.column_dimensions[col].width = 4
+
 
 # Обработка таблицы с периодами
 def markToInt(x):
     marks = ['2','3','4','5']
+    print(x)
     if x in marks:
         return int(x)
-    return x
+    elif x == '':
+        return ''
+    else:
+        return x
+
+# Количество выявленных ошибок
+errors_count = 0
 
 for fio in range(3,len(data)):
     p1 = ['Аттестационный период 1']
@@ -74,6 +89,8 @@ for fio in range(3,len(data)):
         p3 = ['Аттестационный период 3']
     god = ['Год']
     predmeti_p1 = ['']
+
+    ### НУЖНО ДОБАВИТЬ ОБРАБОЧИК ВСТАВКИ ПУСТЫХ ПЕРИОДОВ ###
     
     for i in range(len(data[2])):
         if data[2][i] == 'Аттестационный период 1':
@@ -87,12 +104,12 @@ for fio in range(3,len(data)):
             god.append(markToInt(data[fio][i]))
     
     # Трассировка вывода данных
-    # print(data[fio][0])
-    # print(p1)
-    # print(p2)
-    # if periods == 3:
-    #     print(p3)
-    # print(god)
+    print(data[fio][0])
+    print(p1)
+    print(p2)
+    if periods == 3:
+        print(p3)
+    print(god)
 
     out_sheet.append([data[fio][0]])
 
@@ -103,4 +120,56 @@ for fio in range(3,len(data)):
         out_sheet.append(p3)
     out_sheet.append(god)
 
-out_book.save('test.xlsx')
+    # проверка выставления итоговых оценок
+    cols = 'ABCDEFGHIJKLMNOPQRSTUVWXWZ'
+    for i in range(len(predmeti_p1)-1):
+        if god[i+1] != '':
+            if periods != 3:
+                a = 0
+                b = 0
+                if p1[i+1] == '':
+                    a == 0
+                elif p1[i+1] == 'НПА' or p1[i+1] == 'АЗ':
+                    continue
+                else:
+                    a = p1[i+1]
+
+                if p2[i+1] == '':
+                    b == 0
+                elif p2[i+1] == 'НПА' or p2[i+1] == 'АЗ':
+                    continue
+                else:
+                    b = p2[i+1]
+
+                if a == 0 or b == 0:
+                    avg = a + b
+                else:
+                    avg = ceil((a + b) / 2)
+
+                print('Предмет:',out_sheet[cols[i + 1] + str(2 + (fio - 3) * 5)].value,'| ячейка:', cols[i + 1] + str(5 + (fio - 3) * 5), 'a=',a,'b=',b, '(a+b)/ 2=',(a+b)/2,'avg=',avg,'god=',god[i+1])
+
+                if avg == god[i+1]:
+                    out_sheet[cols[i+1]+str(5+(fio-3)*5)].fill = PatternFill('solid',fgColor='A6F16C')
+                else:
+                    # Закрашиваем ячейку с ошибкой красным
+                    out_sheet[cols[i + 1] + str(5 + (fio - 3) * 5)].fill = PatternFill('solid', fgColor='FF8B73')
+                    errors_count += 1
+                    # Закрашиваем ФИО ученика с ошибкой красным
+                    out_sheet['A' + str(1+(fio - 3) * 5)].fill = PatternFill('solid', fgColor='FF8B73')
+
+    # переворот названий предметов в ячейке на 90 градусов
+    for row in out_sheet['B'+str(2+(fio-3)*5)+':Z'+str(2+(fio-3)*5)]:
+        for cell in row:
+            cell.alignment = Alignment(textRotation=90)
+
+out_sheet.insert_rows(1)
+out_sheet['A1'] = "Количество ошибок:"
+out_sheet['B1'] = errors_count
+if errors_count == 0:
+    out_sheet['A1'].fill = PatternFill('solid', fgColor='A6F16C')
+    out_sheet['B1'].fill = PatternFill('solid', fgColor='A6F16C')
+else:
+    out_sheet['A1'].fill = PatternFill('solid', fgColor='FF8B73')
+    out_sheet['B1'].fill = PatternFill('solid', fgColor='FF8B73')
+
+out_book.save('checked/' + file_name_all_periods + '-ПРОВЕРЕНО('+str(errors_count)+').xlsx')
